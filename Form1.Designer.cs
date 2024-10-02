880ca69f639c101e08115ee03def8971209d633e
Aleco <113533233+VentiPlenti@users.noreply.github.com>
2024-10-02 09:15:18 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace synes
+{
+    partial class frmSynes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSynes));
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pbOutput = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblVersion = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbOutput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(19, 118);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(1451, 43);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(446, 180);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(569, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Go!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pbOutput
+            // 
+            this.pbOutput.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pbOutput.Location = new System.Drawing.Point(19, 232);
+            this.pbOutput.Name = "pbOutput";
+            this.pbOutput.Size = new System.Drawing.Size(1451, 163);
+            this.pbOutput.TabIndex = 2;
+            this.pbOutput.TabStop = false;
+            this.pbOutput.Paint += new System.Windows.Forms.PaintEventHandler(this.pbOutput_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(228, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(327, 75);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Alec\'s scuffed synesthesia translator\r\n\r\n\r\n";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblVersion
+            // 
+            this.lblVersion.AutoSize = true;
+            this.lblVersion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVersion.Location = new System.Drawing.Point(621, 13);
+            this.lblVersion.Name = "lblVersion";
+            this.lblVersion.Size = new System.Drawing.Size(350, 100);
+            this.lblVersion.TabIndex = 4;
+            this.lblVersion.Text = resources.GetString("lblVersion.Text");
+            // 
+            // frmSynes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1508, 450);
+            this.Controls.Add(this.lblVersion);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pbOutput);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtInput);
+            this.Name = "frmSynes";
+            this.Text = "Synesthesia";
+            ((System.ComponentModel.ISupportInitialize)(this.pbOutput)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pbOutput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblVersion;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace synes
+{
+    public partial class frmSynes : Form
+    {
+        public frmSynes()
+        {
+            InitializeComponent();
+            txtInput.Text = "ha";
+        }
+
+        private Color GetColorForLetter(char letter)
+        {
+            switch (char.ToUpper(letter))
+            {
+                case 'A': return Color.FromArgb(255, 188, 0, 10);      // Opaque Red
+                case 'B': return Color.FromArgb(255, 227, 132, 23);   // Opaque Orange
+                case 'C': return Color.FromArgb(255, 32, 40, 250);    // Opaque Blue
+                case 'D': return Color.FromArgb(210, 20, 10, 0);      // Semi-Opaque Black
+                case 'E': return Color.FromArgb(255, 133, 220, 90);     // #85E65E
+                case 'F': return Color.FromArgb(204, 53, 15, 35);     // Semi-Opaque Dark Red
+                case 'G': return Color.FromArgb(255, 29, 110, 36);      // Opaque Dark Green
+                case 'H': return Color.FromArgb(212, 247, 193, 75);    // Semi-Opaque Yellow
+                case 'I': return Color.FromArgb(56, 237, 254, 255);    // Very Transparent Light Blue
+                case 'J': return Color.FromArgb(255, 200, 45, 1);      // Opaque Orange
+                case 'K': return Color.FromArgb(255, 240, 210, 0);     // Opaque Yellow
+                case 'L': return Color.FromArgb(255, 159, 58, 89);     // Opaque Purple
+                case 'M': return Color.FromArgb(255, 129, 11, 22);    // Opaque Purple
+                case 'N': return Color.FromArgb(255, 228, 90, 20);    // Opaque Orange
+                case 'O': return Color.FromArgb(56, 237, 254, 255);     // Very Transparent Light Blue
+                case 'P': return Color.FromArgb(255, 245, 242, 20);    // Opaque Yellow
+                case 'Q': return Color.FromArgb(255, 188, 88, 0);       // Opaque Orange
+                case 'R': return Color.FromArgb(214, 37, 2, 76);        // Semi-Opaque Dark Purple
+                case 'S': return Color.FromArgb(245, 135, 200, 35);      // Semi-Opaque Green
+                case 'T': return Color.FromArgb(255, 0, 11, 160);       // Opaque Blue
+                case 'U': return Color.FromArgb(255, 70, 9, 130);       // Opaque Purple
+                case 'V': return Color.FromArgb(255, 237, 148, 0);      // Opaque Orange
+                case 'W': return Color.FromArgb(255, 122, 58, 74);     // #7A3A4A
+                case 'X': return Color.FromArgb(255, 46, 71, 125);      // Opaque Blue
+                case 'Y': return Color.FromArgb(255, 234, 176, 0);     // Opaque Yellow
+                case 'Z': return Color.FromArgb(255, 160, 72, 22);      // Opaque Brown
+                case 'Ł': return Color.FromArgb(255, 124, 48, 77);      // #7C304D
+                case 'Ę': return Color.FromArgb(255, 117, 199, 85);     // #75C755
+                case 'Ń': return Color.FromArgb(255, 255, 57, 21);      // #FF3915
+                case 'Ś': return Color.FromArgb(255, 177, 249, 68);     // #B1F944
+                case 'Ź': return Color.FromArgb(255, 190, 57, 21);      // #BE3915
+                case 'Ż': return Color.FromArgb(255, 134, 32, 3);       // #862003
+                case 'Ó': return Color.FromArgb(204, 199, 225, 218);     // #C7E1DA (80% transparent)
+                case 'Ą': return Color.FromArgb(255, 228, 38, 51);      // #E42633
+                case '3': return Color.FromArgb(255, 206, 80, 141);     // #CE508D
+
+
+                default: return Color.Black;  // Default color if no mapping exists
+            }
+        }
+
+        private void DrawColoredText(Graphics g, string text, Font font)
+        {
+            float x = 10;
+            float y = 10;
+            float maxWidth = 800; // Set your desired maximum width for a line
+
+            // Split the input text into words
+            foreach (var word in text.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                // Check if the word is not empty
+                if (string.IsNullOrWhiteSpace(word)) continue;
+
+                // Measure the width of the current word
+                float wordWidth = g.MeasureString(word, font).Width;
+
+                // Check if the word fits within the max width
+                if (x + wordWidth > maxWidth)
+                {
+                    // Move to the next line
+                    x = 10; // Reset x to start of the line
+                    y += font.GetHeight(g) + 5; // Move y down for the next line
+                }
+
+                // Draw each letter in color with an outline
+                for (int i = 0; i < word.Length; i++)
+                {
+                    // Get the character and its color
+                    char letter = word[i];
+                    Color letterColor = GetColorForLetter(letter);
+                    Color firstLetterColor = GetColorForLetter(word[0]); // 
+                    Color outlineColor = Color.FromArgb(170, firstLetterColor); // Opaque outline
+
+                    // Measure the width of the current letter
+                    float letterWidth = g.MeasureString(letter.ToString(), font).Width;
+
+                    // Create a GraphicsPath for the current letter
+                    using (GraphicsPath path = new GraphicsPath())
+                    {
+                        path.AddString(letter.ToString(), font.FontFamily, (int)font.Style, font.Size, new PointF(x, y), StringFormat.GenericDefault);
+
+                        // Draw the filled character
+                        g.DrawString(letter.ToString(), font, new SolidBrush(letterColor), x, y);
+                        // Draw the outline for the character
+                        g.DrawPath(new Pen(outlineColor, 4), path);
+                    }
+
+                    // Move x position for the next letter
+                    x += letterWidth;
+                }
+
+                // Add space between words
+                x += g.MeasureString(" ", font).Width;
+            }
+        }
+
+
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pbOutput.Invalidate(); // Request to redraw the PictureBox
+        }
+
+        private void pbOutput_Paint(object sender, PaintEventArgs e)
+        {
+            Console.WriteLine("Paint event triggered");
+            DrawColoredText(e.Graphics, txtInput.Text, new Font("Arial", 24));
+        }
+
+        private void txtInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
